8273e41bc19a4352104725abf1ce63754ce9ba96
Fresnel Amahowe <fresnel.amahowe@gmail.com>
2025-08-07 05:37:45 +0200

    fix: base path for Netlify

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/PORTFOLIO/", // 
+  base: "/", // Assure-toi que 'PORTFOLIO' correspond au nom exact de ton repo sur GitHub
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
